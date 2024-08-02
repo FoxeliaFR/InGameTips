a2b93550af0ddb80d84f919f76584f4fe5845f2c
Zarinoow <77458474+Zarinoow@users.noreply.github.com>
2024-08-02 17:54:19 +0200

    Now using seconds

--- src/main/java/fr/foxelia/ingametips/config/InGameTipsCommonConfigs.java
+++ src/main/java/fr/foxelia/ingametips/config/InGameTipsCommonConfigs.java
@@ -1,5 +1,6 @@
 package fr.foxelia.ingametips.config;
 
+import fr.foxelia.ingametips.tip.PopUp;
 import net.minecraftforge.common.ForgeConfigSpec;
 
 import java.util.ArrayList;
@@ -23,8 +24,8 @@ public class InGameTipsCommonConfigs {
     static {
         COMMON_BUILDER.push("Common configuration settings for InGameTips");
 
-        scheduleTime = COMMON_BUILDER.comment("The time in minutes between each tip. Set to 0 to disable scheduled tips.")
-                .defineInRange("scheduleTime", 5, 0, Integer.MAX_VALUE);
+        scheduleTime = COMMON_BUILDER.comment("The time in seconds between each tip. Set to 0 to disable scheduled tips.\nExample: 60 (1 minute), 90 (1 minute 30 seconds), 300 (5 minutes)")
+                .defineInRange("scheduleTime", 300, 0, Integer.MAX_VALUE);
         disabledNamespaces = COMMON_BUILDER.comment("List of mod namespaces to disable tips from.\nExample: [\"modid\", \"datapackid\"]")
                 .defineList("disabledNamespaces", new ArrayList<>(), obj -> obj instanceof String);
         syncTipsWithAllPlayers = COMMON_BUILDER.comment("All players receive the same tip at the same time")

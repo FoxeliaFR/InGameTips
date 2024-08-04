8062a7fe724c1a2642c6f72287115531cbca58b9
Zarinoow <77458474+Zarinoow@users.noreply.github.com>
2024-08-04 17:44:54 +0200

    Removed debug command

--- src/main/java/fr/foxelia/ingametips/commands/CommandRegistry.java
+++ src/main/java/fr/foxelia/ingametips/commands/CommandRegistry.java
@@ -1,7 +1,6 @@
 package fr.foxelia.ingametips.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
-import fr.foxelia.ingametips.test.TestCommand;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraftforge.event.AddReloadListenerEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -10,7 +9,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 public class CommandRegistry {
 
     private static void registerCommands(CommandDispatcher<CommandSourceStack> dispatcher) {
-        TestCommand.register(dispatcher); // REMOVE THIS LINE BEFORE PRODUCTION
         TipCommand.register(dispatcher);
     }
 

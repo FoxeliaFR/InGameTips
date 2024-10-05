3dbbc5dc1a0019a26e03e5be547b3338ba840a0a
Zarinoow <77458474+Zarinoow@users.noreply.github.com>
2024-10-05 20:06:56 +0200

    Renamed to match the mod name

--- quilt/src/main/java/fr/foxelia/igtips/quilt/InGameTipsQuilt.java
+++ quilt/src/main/java/fr/foxelia/igtips/quilt/InGameTipsQuilt.java
@@ -5,7 +5,7 @@ import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
 import fr.foxelia.igtips.InGameTips;
 
-public final class ExampleModQuilt implements ModInitializer {
+public final class InGameTipsQuilt implements ModInitializer {
     @Override
     public void onInitialize(ModContainer mod) {
         // Run our common setup.

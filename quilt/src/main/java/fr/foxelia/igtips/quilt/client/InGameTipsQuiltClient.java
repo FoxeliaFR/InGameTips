5363dffb2c4e7d53887c2a93b19bb5ff0be42b5c
Zarinoow <77458474+Zarinoow@users.noreply.github.com>
2024-11-03 18:30:11 +0100

    Send language to the server on player join

---
+++ quilt/src/main/java/fr/foxelia/igtips/quilt/client/InGameTipsQuiltClient.java
@@ -0,0 +1,14 @@
+package fr.foxelia.igtips.quilt.client;
+
+import fr.foxelia.igtips.quilt.network.PlayerLanguagePacket;
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.client.ClientModInitializer;
+import org.quiltmc.qsl.networking.api.client.ClientPlayConnectionEvents;
+
+public class InGameTipsQuiltClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient(ModContainer mod) {
+        // Update the player's language when they join the server
+        ClientPlayConnectionEvents.JOIN.register(((handler, sender, client) -> PlayerLanguagePacket.sendLanguageToServer(client.getLanguageManager().getLanguage())));
+    }
+}

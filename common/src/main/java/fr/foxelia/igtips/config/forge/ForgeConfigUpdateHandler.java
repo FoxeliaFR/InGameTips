fa8d6c24440c4f864e38f3f59fa982bd38b35941
Zarinoow <77458474+Zarinoow@users.noreply.github.com>
2024-12-27 00:37:28 +0100

    Synchronize to all op player on file update

--- common/src/main/java/fr/foxelia/igtips/config/forge/ForgeConfigUpdateHandler.java
+++ common/src/main/java/fr/foxelia/igtips/config/forge/ForgeConfigUpdateHandler.java
@@ -1,13 +1,21 @@
 package fr.foxelia.igtips.config.forge;
 
 import fr.foxelia.igtips.InGameTips;
+import fr.foxelia.igtips.config.CommonConfigManager;
 import fr.foxelia.igtips.schedule.ScheduleManager;
 import net.minecraftforge.fml.config.ModConfig;
 
 public class ForgeConfigUpdateHandler {
     public static void configChanged(ModConfig config) {
         if (config.getFileName().equals(InGameTips.MOD_ID + "-common.toml")) {
-            if (InGameTips.SERVER != null) ScheduleManager.INSTANCE.refresh();
+            if (InGameTips.SERVER != null) {
+                // Refresh the schedule manager
+                ScheduleManager.INSTANCE.refresh();
+                // Update the config to all players
+                InGameTips.SERVER.getPlayerManager().getPlayerList().forEach(player -> {
+                    CommonConfigManager.syncConfigToPlayer(player);
+                });
+            }
         }
     }
 }

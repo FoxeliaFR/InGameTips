7805e5cac9e50e36ce3a455f6f4f46bbd901d344
Zarinoow <77458474+Zarinoow@users.noreply.github.com>
2024-12-26 22:11:43 +0100

    Added setters to allow editing the config

--- common/src/main/java/fr/foxelia/igtips/client/config/IClientInGameTipsConfig.java
+++ common/src/main/java/fr/foxelia/igtips/client/config/IClientInGameTipsConfig.java
@@ -2,6 +2,18 @@ package fr.foxelia.igtips.client.config;
 
 public interface IClientInGameTipsConfig {
 
+    default void setTipsEnabled(boolean enabled) {
+        throw new UnsupportedOperationException("Cannot modify config values in a read-only config");
+    }
+
+    default void setSoundEnabled(boolean enabled) {
+        throw new UnsupportedOperationException("Cannot modify config values in a read-only config");
+    }
+
+    default void setMaxLines(int maxLines) {
+        throw new UnsupportedOperationException("Cannot modify config values in a read-only config");
+    }
+
     boolean isTipsEnabled();
     boolean isSoundEnabled();
     int getMaxLines();

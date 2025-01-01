09d41257af45b5076e4ae08058826979e3fb5746
Zarinoow <77458474+Zarinoow@users.noreply.github.com>
2025-01-01 23:12:19 +0100

    Insulating client code on the client-side to prevent server crash

--- forge/src/main/java/fr/foxelia/igtips/forge/InGameTipsForge.java
+++ forge/src/main/java/fr/foxelia/igtips/forge/InGameTipsForge.java
@@ -2,8 +2,11 @@ package fr.foxelia.igtips.forge;
 
 import dev.architectury.platform.forge.EventBuses;
 import fr.foxelia.igtips.InGameTips;
+import fr.foxelia.igtips.forge.client.addons.cloth.InGameTipsClothConfigIntegration;
 import fr.foxelia.igtips.forge.event.ModConfigReloadingEventHandler;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -20,10 +23,16 @@ public final class InGameTipsForge {
 
         // Run our common setup.
         InGameTips.init();
-        InGameTips.initClient();
+        // Run our client setup.
+        DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> () -> InGameTips.initClient());
 
         // Register the mod config reloading event handler.
         modEventBus.addListener(ModConfigReloadingEventHandler::onConfigChanged);
+        modEventBus.addListener(this::onClientSetup);
+    }
+
+    private void onClientSetup(FMLClientSetupEvent event) {
+        InGameTipsClothConfigIntegration.registerConfigScreen();
     }
 
 }
